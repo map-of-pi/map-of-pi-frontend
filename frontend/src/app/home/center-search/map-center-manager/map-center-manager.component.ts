00e546e62289ce96444b726233c177ab0be92356
Darin Hajou <darinhajou@gmail.com>
2024-04-03 14:20:06 +0200

    Add geo location and dynamic translations to MapCenterManager

--- frontend/src/app/home/center-search/map-center-manager/map-center-manager.component.ts
+++ frontend/src/app/home/center-search/map-center-manager/map-center-manager.component.ts
@@ -3,7 +3,6 @@ import { Router, RouterModule } from '@angular/router';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { TranslateService } from '@ngx-translate/core';
 
-import axios from 'axios';
 import { Subscription } from 'rxjs';
 import { NGXLogger } from 'ngx-logger';
 import { Map, marker, Layer } from 'leaflet';
@@ -22,5 +21,66 @@ import { GeolocationService } from '../../../core/service/geolocation.service';
 })
 
 export class MapCenterManagerComponent {
+  layer?: Layer;
+  map!: Map;
+  options;
+  id: string = '65e2d67a38e1e60afd74378d';
+  navigator: Router = inject(Router);
+  showPopup: boolean = false;
+  searchBarQuery: string = '';
+  userPositions: any[] = [];
+  
+  // Translation strings
+  userLocation!: string;
+  mobileTransporationDistanceMessage!: string;
+  mobileTransportationTimeMessage!: string;
+  cancelButton!: string;
+  middleClickedMessage!: string;
+  unknownMarkerClickedMessage!: string;
+  
+  private userMarker: any;
+  private langChangeSubscription: Subscription;
+  private geolocationSubscription: Subscription;
 
+constructor(
+  private readonly geolocationService: GeolocationService,
+  private translateService: TranslateService,
+  private logger: NGXLogger) {
+    
+  this.options = this.geolocationService.getMapOptions();
+  this.geolocationSubscription = this.geolocationService.geolocationTriggerEvent$.subscribe(() => {
+    this.locateMe();
+  });
+
+  this.langChangeSubscription = this.translateService.onLangChange.subscribe(() => {
+    this.updateTranslatedStrings();
+  });
+}
+
+async ngOnInit(): Promise<void> {
+  try {
+
+    // Wait for translation update before adding coordinates to the map
+    this.updateTranslatedStrings();
+
+  } catch (error) {
+    this.logger.error(error);
+    }
+  }
+
+  onMapReady(map: Map): void {
+    this.map = map;
+  }
+
+  locateMe(): void {
+  }
+
+  private updateTranslatedStrings(): void {
+    this.userLocation = this.translateService.instant('MAP.USER_LOCATION');
+    this.mobileTransporationDistanceMessage = this.translateService.instant('MAP.MOBILE_TRANSPORTATION_DISTANCE_MESSAGE');
+    this.mobileTransportationTimeMessage = this.translateService.instant('MAP.MOBILE_TRANSPORTATION_TIME_MESSAGE');
+    this.cancelButton = this.translateService.instant('MAP.BUTTONS.CANCEL'); 
+    this.middleClickedMessage = this.translateService.instant('MAP.MIDDLE_CLICKED_MESSAGE');
+    this.unknownMarkerClickedMessage = this.translateService.instant('MAP.UNKNOWN_MARKER_CLICKED_MESSAGE');
+  }
 }
