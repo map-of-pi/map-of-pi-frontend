bf598a9ded972a1fd806b63af8d08facec34c17a
Darin Hajou <darinhajou@gmail.com>
2024-04-03 14:04:52 +0200

    Add necessary imports for MapCenterManager component

--- frontend/src/app/home/center-search/map-center-manager/map-center-manager.component.ts
+++ frontend/src/app/home/center-search/map-center-manager/map-center-manager.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, OnDestroy, EventEmitter, Output, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { TranslateService } from '@ngx-translate/core';
+
+import { Subscription } from 'rxjs';
+import { NGXLogger } from 'ngx-logger';
+import { Map, marker, Layer } from 'leaflet';
+import * as L from 'leaflet';
+import 'leaflet-routing-machine';
+
+import { GeolocationService } from '../../../core/service/geolocation.service';
+// import { CustomMarkerOptions } from './marker-options.interface';
 
 @Component({
   selector: 'app-map-center-manager',
   standalone: true,
-  imports: [],
+  imports: [LeafletModule],
   templateUrl: './map-center-manager.component.html',
   styleUrl: './map-center-manager.component.scss'
 })
