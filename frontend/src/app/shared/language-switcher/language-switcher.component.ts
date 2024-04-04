d8e3598e3b57e399d8d738c2d95b1d3fc336c90c
swoocn <swooshinn@gmail.com>
2024-04-04 19:18:50 -0400

    Misc. NG_HAU adjustments.

--- frontend/src/app/shared/language-switcher/language-switcher.component.ts
+++ frontend/src/app/shared/language-switcher/language-switcher.component.ts
@@ -27,6 +27,7 @@ export class LanguageSwitcherComponent {
     { code: 'en', label: 'EN', name: 'English', translation: 'English', imageUrl: '../../../assets/images/shared/language/en.svg' },
     { code: 'es', label: 'ES', name: 'Spanish', translation: 'Español', imageUrl: '../../../assets/images/shared/language/es.svg' },
     { code: 'ko', label: 'KO', name: 'Korean', translation: '한국어', imageUrl: '../../../assets/images/shared/language/ko.svg' },
+    { code: 'ng_hau', label: 'NG/HAU', name: 'Nigerian Hausa', translation: 'Hausa', imageUrl: '../../../assets/images/shared/language/ng.svg' },
     { code: 'ng_yor', label: 'NG/YOR', name: 'Nigerian Yoruba', translation: 'Yorùbá', imageUrl: '../../../assets/images/shared/language/ng.svg' }
   ];
 
