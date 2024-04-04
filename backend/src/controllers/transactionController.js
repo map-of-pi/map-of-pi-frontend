fbd8517a0bf4a341cc80b7f85f500a079be071c6
swoocn <swooshinn@gmail.com>
2024-04-04 17:04:07 -0400

    Added modded winston logging for transactionController.

--- backend/src/controllers/transactionController.js
+++ backend/src/controllers/transactionController.js
@@ -1,13 +1,19 @@
 const Order = require("../models/orderModel");
 const logger = require("../../logger");
+const initializeLogger = require("../../logger");
+
+let logger;
+
+initializeLogger().then((initializedLogger) => {
+  logger = initializedLogger;
+});
 
 const getUserTransactions = async (req, res) => {
-    const currentUser = req.currentUser;
+  const currentUser = req.currentUser;
 
   try {
-  
     const transactions = await Order.find({ user: currentUser.uid }).sort({ createdAt: 1 })
-    logger.info("Successfully fetched user transactions");
+    logger.debug("Successfully fetched user transactions");
     return res.status(200).json({ transactions });
   } catch (error) {
     logger.error("Error getting user transactions:", error.message);
