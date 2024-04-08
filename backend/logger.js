06b2536958451f6ddbca2ecf49212b205b659786
swoocn <swooshinn@gmail.com>
2024-04-08 17:54:37 -0400

    Remove backend and unrelated artifacts.

--- backend/logger.js
+++
@@ -1,7 +0,0 @@
-const winston = require('winston');
-const configureLogging = require('./logging-config');
-
-const loggingConfig = configureLogging();
-const logger = winston.createLogger(loggingConfig);
-
-module.exports = logger;
